70c37c0467e50aa37e01172cdb39881e2fe1e856
marco <marco.apolinario.l@gmail.com>
2022-06-23 03:15:17 -0400

    Updating custom layer

--- Inference_pytorch/inference.py
+++ Inference_pytorch/inference.py
@@ -127,10 +127,10 @@ for i, (data, target) in enumerate(test_loader):
     with torch.no_grad():
         data, target = Variable(data), Variable(target)
         output = modelCF(data)
-        test_loss_i = criterion(output, target)
-        test_loss += test_loss_i.data
-        pred = output.data.max(1)[1]  # get the index of the max log-probability
-        correct += pred.cpu().eq(indx_target).sum()
+        # test_loss_i = criterion(output, target)
+        # test_loss += test_loss_i.data
+        # pred = output.data.max(1)[1]  # get the index of the max log-probability
+        # correct += pred.cpu().eq(indx_target).sum()
     if i==0:
         hook.remove_hook_list(hook_handle_list)
     break
