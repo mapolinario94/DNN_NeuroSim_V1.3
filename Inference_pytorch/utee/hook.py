dd6d0c55b096366fe296debc6adf3e0e40766640
mapolina <marco.apolinario.l@gmail.com>
2022-06-24 13:57:28 -0400

    Creating new layer

--- Inference_pytorch/utee/hook.py
+++ Inference_pytorch/utee/hook.py
@@ -61,8 +61,10 @@ def stretch_input(input_matrix,window_size = 5,padding=(0,0),stride=(1,1)):
     # torch.random.manual_seed(1234)
     output_matrix = np.zeros((input_shape[0],int(item_num),input_shape[1]*window_size*window_size))
     iter = 0
-    for i in range( int(((input_shape[2] + 2*padding[0] - window_size) / stride[0] + 1))):
-        for j in range( int(((input_shape[3] + 2*padding[1] - window_size) / stride[1] + 1))):
+    # for i in range( int(((input_shape[2] + 2*padding[0] - window_size) / stride[0] + 1))):
+    #     for j in range( int(((input_shape[3] + 2*padding[1] - window_size) / stride[1] + 1))):
+    for i in range(input_shape[2] - window_size + 1):
+        for j in range(input_shape[3] - window_size + 1):
             for b in range(input_shape[0]):
                 output_matrix[b,iter,:] = input_matrix[b, :, i:i+window_size,j: j+window_size].reshape(input_shape[1]*window_size*window_size)
             iter += 1
