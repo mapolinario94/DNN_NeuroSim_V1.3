59d7bf7630c94b134c0abd313b3588182cfe722d
mapolina <marco.apolinario.l@gmail.com>
2022-07-14 13:16:55 -0400

    simulatuion pipeline 5bit ADC

--- Inference_pytorch/NeuroSIM/Param.cpp
+++ Inference_pytorch/NeuroSIM/Param.cpp
@@ -144,8 +144,8 @@ Param::Param() {
 	relaxArrayCellHeight = 0;           // relax ArrayCellHeight or not
 	relaxArrayCellWidth = 0;            // relax ArrayCellWidth or not
 
-	numColMuxed = 8;                    // How many columns share 1 ADC (for eNVM and FeFET) or parallel SRAM
-	levelOutput = 32;                   // # of levels of the multilevelSenseAmp output, should be in 2^N forms; e.g. 32 levels --> 5-bit ADC
+	numColMuxed = 1;                    // How many columns share 1 ADC (for eNVM and FeFET) or parallel SRAM
+	levelOutput = 4;                   // # of levels of the multilevelSenseAmp output, should be in 2^N forms; e.g. 32 levels --> 5-bit ADC
 	cellBit = 1;                        // precision of memory device
 
 	/*** parameters for SRAM ***/
